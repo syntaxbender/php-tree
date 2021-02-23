87bb05505cd4da5ec17ebd3f296eed6aec57dda0
syntaxbender <mail@syntaxbender.com>
2021-02-24 01:54:42 +0300

    tree

--- example.php
+++ example.php
@@ -1,39 +1,44 @@
 <?php
-$executionStartTime = microtime(true);
-require_once __DIR__."/sbBasicTree.php";
-$test = [
-	5=>["CatID"=>5,"PrID"=>1,"Title"=>"Bilgisayar"],
-	6=>["CatID"=>6,"PrID"=>1,"Title"=>"Telefon"],
-	7=>["CatID"=>7,"PrID"=>5,"Title"=>"laptop"],
-	8=>["CatID"=>8,"PrID"=>5,"Title"=>"masaüstü"],
-	9=>["CatID"=>9,"PrID"=>8,"Title"=>"anakart"],
-	10=>["CatID"=>10,"PrID"=>8,"Title"=>"işlemci"],
-	11=>["CatID"=>11,"PrID"=>6,"Title"=>"kulaklık"],
-	12=>["CatID"=>12,"PrID"=>6,"Title"=>"tel batarya"],
-	13=>["CatID"=>13,"PrID"=>2,"Title"=>"peynir"],
-	14=>["CatID"=>14,"PrID"=>2,"Title"=>"et"],
-	15=>["CatID"=>15,"PrID"=>2,"Title"=>"şarküteri"],
-	16=>["CatID"=>16,"PrID"=>2,"Title"=>"balık"],
-	17=>["CatID"=>17,"PrID"=>3,"Title"=>"outdoor"],
-	18=>["CatID"=>18,"PrID"=>3,"Title"=>"indoor"],
-	19=>["CatID"=>19,"PrID"=>17,"Title"=>"bisiklet"],
-	20=>["CatID"=>20,"PrID"=>17,"Title"=>"yüzme"],
-	21=>["CatID"=>21,"PrID"=>19,"Title"=>"gidon"],
-	22=>["CatID"=>22,"PrID"=>19,"Title"=>"sele"],
-	1=>["CatID"=>1,"PrID"=>0,"Title"=>"Elektronik"],
-	2=>["CatID"=>2,"PrID"=>0,"Title"=>"Gıda"], 
-	3=>["CatID"=>3,"PrID"=>0,"Title"=>"Spor"],
-	4=>["CatID"=>4,"PrID"=>0,"Title"=>"Giyim"]
-];
-$MDTreeArray = new sbBasicTree($test); 
-echo "TreeArray : <br><br><br>\n\n\n";
-print_r($MDTreeArray->getTreeArray());
-echo "<br><br><br>\n\n\nChildren : <br><br><br>\n\n\n";
-print_r($MDTreeArray->getChildren(1));
-echo "<br><br><br>\n\n\nParents : <br><br><br>\n\n\n";
-print_r($MDTreeArray->getParents(19));
-
-$executionEndTime = microtime(true);
-$seconds = $executionEndTime - $executionStartTime;
-echo "This script took $seconds to execute.";
+class sbBasicTree{
+	public $newarr = [];
+	public $arr;
+	function __construct($arr) {
+		$this->arr=$arr;
+		foreach($this->arr as $item){
+			$parents = $this->getParents($item["CatID"]); // önce parentlere doğru çıkıyorum sonra onları terse çeviriyorum.
+			$ref = &$this->newarr;
+			$level = 0;
+			foreach ($parents as $parent){ // sonra bi daha parentlerden aşağı inip vakit kaybediyorum mükemmel xd
+				$level++;
+				$ref = &$ref[$parent]["SubCats"];
+			}
+			$ref['CatID'] = $item['CatID'];
+			$ref['Title'] = $item['Title'];
+			$ref['Level'] = $level;
+		}
+	}
+	function getParents($id){
+		$temp = [];
+		$temp[] = $id;
+		while($this->arr[$id]["PrID"] != 0){
+			$temp[] = $this->arr[$id]["PrID"];
+			$id=$this->arr[$id]["PrID"];
+		}
+		$temp = array_reverse($temp);
+		return $temp;
+	}
+	function getChildren($id,$recursiveCarrier=[]){
+		foreach($this->arr as $item){
+			if($item["PrID"] == $id){
+				$recursiveCarrier[] = $item["CatID"];
+				$oyy = $this->getChildren($item["CatID"],$recursiveCarrier);
+				$recursiveCarrier = $oyy;
+			}
+		}
+		return $recursiveCarrier;
+	}
+	function getTreeArray(){
+		return $this->newarr;
+	}
+}
 ?>
